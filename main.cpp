7fa11199846e697941131b7ac36bf301d0d26b16
kandadadada <86107451+kandadadada@users.noreply.github.com>
2023-02-21 18:43:15 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include "QEI.h"
 #include "DCMotor.h"
 
-#define RESOLUTION 100 //なぜか100が一番うまくいく
-#define INT_TIME 0.005 //割り込みスパン
+#define RESOLUTION 310//なぜかこの値がちょうどいい
+#define INT_TIME 0.05//クロックのスパン
 
-//モータの設定
+
 DCMotor dcMotorR(D9, D10);
 DCMotor dcMotorL(D7,D2);
 
@@ -13,13 +13,18 @@ DCMotor dcMotorL(D7,D2);
 Ticker timer;
 Timer T;
 
-QEI Enc1(D12,A3,NC,RESOLUTION,&T,QEI::X4_ENCODING);
-//QEI Enc2(p5,p6,NC,RESOLUTION,&T,QEI::X4_ENCODING);
+QEI Enc1(A3,D12,NC,RESOLUTION,&T,QEI::X4_ENCODING);
+QEI Enc2(A1,A0,NC,RESOLUTION,&T,QEI::X4_ENCODING);
 Serial pc(USBTX,USBRX);
 
-int encountRev = 0;
-double encountAngle=0;
-int encountPulse=0;
+int encountRev1 = 0;
+double encountAngle1=0;
+int encountPulse1=0;
+
+int encountRev2 = 0;
+double encountAngle2=0;
+int encountPulse2=0;
+
 
 
 void timer_warikomi()
@@ -27,16 +32,20 @@ void timer_warikomi()
     
     //static float pre_sp1 = 0;
     
-    encountRev = Enc1.getAng_rev();//回転数（整数）
-    encountAngle = Enc1.getAngle();//回転角度
-    encountPulse = Enc1.getPulses();//出力された合計パルス
+    encountRev1 = Enc1.getAng_rev();//回転数
+    encountAngle1 = Enc1.getAngle();//回転角度
+    encountPulse1 = Enc1.getPulses();//合計パルス
+
+    encountRev2 = Enc2.getAng_rev();
+    encountAngle2 = Enc2.getAngle();
+    encountPulse2 = Enc2.getPulses();
     //encount2 = Enc2.getPulses();
     //if (encount1 > encount2) encount_rot = encount1;
    // else encount_rot = encount2;
     
     //float rot_sp1 = encount_rot/multi/ppr*pull_ratio; 
     //spd1 = (rot_sp1 - pre_sp1)/INT_TIME/(48*4);
-    pc.printf("rev=%d,angle=%f,pulse=%d\n", encountRev,encountAngle,encountPulse);
+    pc.printf("rev1=%d,angle1=%f,pulse1=%d  Rev2=%d,angle2=%f,pulse2=%d \n", encountRev1,encountAngle1,encountPulse1,encountRev2,encountAngle2,encountPulse2);
         
 }
 
@@ -47,8 +56,11 @@ int main() {
     
     while(1) {  
         // 正転
-        dcMotorL.forward(0.5);
-        dcMotorR.forward(0.5);
+        dcMotorL.forward(1);
+        dcMotorR.forward(1);
         wait(5);    
+        
     }
 }
+
+
